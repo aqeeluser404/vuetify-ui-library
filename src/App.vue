af441cd4a446d589c934cfca976c1f4ffc408dc1
Aqeel <aqeelhanslo@gmail.com>
2024-04-18 14:42:46 +0200

    Created sidebar with avatar and icon list

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <!-- All vuetify components installed globally -->
+<!-- https://vuetifyjs.com/en/features/application-layout/  24:36 -->
+
+<script setup>
+  import SideBar from './components/SideBar.vue'
+
+</script>
+
 <template>
   <v-app>
     <!-- SIDEBAR -->
+    <SideBar />
     <v-main>
       <!-- CARDS -->
     </v-main>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-navigation-drawer
+        expand-on-hover
+        rail
+    >
+        <v-list>
+            <v-list-item
+                prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+                subtitle="aqeelhanslo@gmailcom"
+                title="Aqeel Hanslo"
+            />
+        </v-list>
+
+        <v-divider />
+
+        <v-list density="compact" nav>
+
+            <v-list-item
+                prepend-icon="mdi-image"
+                title="Color Gallery"
+                value="color"
+            />
+
+            <v-list-item
+                prepend-icon="mdi-dialpad"
+                title="Greyspace Gallery"
+                value="color"
+            />
+
+
+        </v-list>
+
+    </v-navigation-drawer>
+</template>
