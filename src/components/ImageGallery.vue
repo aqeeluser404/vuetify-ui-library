de999235ec52e41e8dc90a2d29864e33c183e1c5
Aqeel <aqeelhanslo@gmail.com>
2024-04-23 22:43:40 +0200

    added loading animation

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -5,9 +5,26 @@
             cols="1"
         >
             <v-card>
+                <!-- image card -->
                 <v-img
                     :src="`https://picsum.photos/500/300?images=${n * 5 + 10}`"
-                />
+                    aspect-ratio="1"
+                    cover
+                >
+                    <!-- loading animation -->
+                    <template v-slot:placeholder>
+                        <v-row
+                            class="fill-height ma-0"
+                            align="center"
+                            justify="center"
+                        >
+                            <v-progress-circular 
+                                indeterminate
+                                color="grey-lighten-5"
+                            ></v-progress-circular >
+                        </v-row>
+                    </template>
+                </v-img>
             </v-card>
         </v-col>
     </v-row>
